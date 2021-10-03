51195f5bfa5ade388d44d9cb4392d12abe04dcfd
Temich93s <nil>
2021-10-04 09:37:43 +1000

    Пройдена тема: Определение базового класса

--- Inheritance/main.swift
+++ Inheritance/main.swift
@@ -9,3 +9,18 @@ import Foundation
 
 print("Hello, World!")
 
+//MARK: Определение базового класса
+print("\n//Определение базового класса")
+
+class Vehicle {
+    var currentSpeed = 0.0
+    var description: String {
+        return "движется на скорости \(currentSpeed) миль в час"
+    }
+    func makeNoise() {
+        //ничего не делаем, так как не каждый транспорт шумит
+    }
+}
+
+let someVehicle = Vehicle()
+print("Транспорт: \(someVehicle.description)")
